e5e954a299a80adb606c881afb0560617225eda8
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-03-08 12:51:53 -0500

    add dom for Form components

--- src/form/Form.js
+++ src/form/Form.js
@@ -12,9 +12,43 @@ export default class Form extends Component {
     
     render() {
         return (
-            <div>
-                
-            </div>
+            <div className="form-container">
+            <form className="register-form" autoComplete="off">
+                {/* Uncomment the next line to show the success message */}
+                {/* <div className="success-message">Success! Thank you for registering</div> */}
+                <input
+                    id="first-name"
+                    className="form-field"
+                    type="text"
+                    placeholder="First Name"
+                    name="firstName"
+                />
+                {/* Uncomment the next line to show the error message */}
+                {/* <span id="first-name-error">Please enter a first name</span> */}
+                <input
+                    id="last-name"
+                    className="form-field"
+                    type="text"
+                    placeholder="Last Name"
+                    name="lastName"
+                />
+                {/* Uncomment the next line to show the error message */}
+                {/* <span id="last-name-error">Please enter a last name</span> */}
+                <input
+                    id="email"
+                    className="form-field"
+                    type="text"
+                    placeholder="Email"
+                    name="email"
+                />
+                {/* Uncomment the next line to show the error message */}
+                {/* <span id="email-error">Please enter an email address</span> */}
+                <button className="form-field" type="submit">
+                    Register
+                </button>
+            </form>
+        </div>
+
         )
     }
 }

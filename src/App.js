47842a16f84bede91c73a0d1309b02ee5a3e9db3
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-03-15 10:00:53 -0400

    update whith router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import Ticket from './ticket/Ticket';
 import Form from './form/Form';
 import Quiz from './quiz/Quix';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Form/>
-      <Quiz/>
-      <Ticket/>
+      <Routes>
+        <Route path='/' element={<Form/>}/>
+        <Route path='/quiz' element={<Quiz/>}/>
+        <Route path='/ticket' element={<Ticket/>}/>
+      </Routes>
     </div>
   );
 }
